13a89579b343d2c2dc00d16457fd4bc41464d631
Larry Madeo <lmadeo76@gmail.com>
2017-06-19 06:46:41 +0000

    added create_ops func, _printf now returns int
    create_ops and _strdup are part of my continuing efforts to
    dynamically allocate my array of op_t structs.  Why? To make it
    easier to pass them around from in a struct of type params,
    with the ultimate goal of making main _printf function as short as
    possible

--- _printf.c
+++ _printf.c
@@ -3,27 +3,11 @@
 /*
  *
  *
- **
+ **/
 op_t *create_ops(op_t *ops)
 {
-	op_t *ops
-}
-**/
-/**
- * _printf - simple function duplicates printf functionality
- * @format: format string just like the standard printf
- *          ... followed by variable number of parameters ... just like printf!
- * Return: int representing number of printed characters
- **/
-
-void _printf(const char *format, ...)
-{
-	params_t p;
-	va_list valist;
-	op_t *ops;
-
-	ops = malloc(sizeof(op_t) * NUMBER_OF_OPS);
-	*ops = {
+	int i = 0, len = 0;
+	op_t pre_ops[] = {
 		{"c", op_char},
 		{"i", op_int},
 		{"%", op_percent},
@@ -31,6 +15,34 @@ void _printf(const char *format, ...)
 		/* {"s", op_string}, */
 		{NULL, NULL}
 	};
+	while (pre_ops[len].op)
+		len++;
+	ops = malloc(sizeof(op_t) * (len + 1));
+	while (pre_ops[i].op)
+	{
+		ops[i].op = _strdup(pre_ops[i].op);
+		ops[i].f = pre_ops[i].f;
+		i++;
+	}
+	ops[i].op = NULL;
+	ops[i].f = NULL;
+	return (ops);
+}
+
+/**
+ * _printf - simple function duplicates printf functionality
+ * @format: format string just like the standard printf
+ *          ... followed by variable number of parameters ... just like printf!
+ * Return: int representing number of printed characters
+ **/
+
+int _printf(const char *format, ...)
+{
+	params_t p;
+	va_list valist;
+	op_t *ops;
+
+	ops = create_ops(ops);
 	va_start(valist, format); /* past this point, only use p->valist */
 	init_params(&p, format, &ops, valist);
 	while (p.format[p.dex])
@@ -44,4 +56,5 @@ void _printf(const char *format, ...)
 	printf("\n");
 	va_end(valist);
 	va_end(p.valist);
+	return (666); /* should return number of printed chars */
 }
