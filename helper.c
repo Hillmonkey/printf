cf7b8ed7fd460746431450135df8130d5572f9a0
Larry Madeo <lmadeo76@gmail.com>
2017-06-20 01:00:13 +0000

    importing format string to init_params

--- helper.c
+++ helper.c
@@ -1,14 +1,26 @@
 #include "holberton.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+			return (write(1, &c, 1));
+}
+
+/**
  * _strdup - make a copy of a string
  * @src: source string
  * Return: a string that is a copy of the source string
  **/
 
-char *_strdup(char *src)
+char *_strdup(const char *src)
 {
-	char *str;
+	char *str, *tmp;
 	char *p;
 	int len = 0;
 
@@ -17,11 +29,11 @@ char *_strdup(char *src)
 	str = malloc(len + 1);
 	if (!str)
 		exit(EXIT_FAILURE);
-	p = str;
+	p = tmp = str;
 	while (*src)
 		*p++ = *src++;
 	*p = '\0';
-	return (str);
+	return (tmp);
 }
 /**
  * init_params - initialize all elements of a param struct
@@ -33,11 +45,10 @@ char *_strdup(char *src)
  **/
 int init_params(params_t *p, const char *format, op_t **ops, va_list valist)
 {
-	UNUSED(format);
 	UNUSED(ops);
 	p->dex = 0;
 	p->counter = 0;
-	/* _strdup(format, p->format); */
+	p->format = _strdup(format);
 	va_copy(p->valist, valist);
 	/* va_start(p->valist, p->format); */
 	return (0);
@@ -51,7 +62,7 @@ int choose_op(params_t *p)
 {
 	int j = 0;
 
-	printf("%s", p->ops[j].op);
+	/* printf("%s", p->ops[j].op); */
 	while (p->ops[j].f)
 	{
 		if (p->ops[j].op[0] == p->format[p->dex])
