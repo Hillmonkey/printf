149d86b3ee6d5dcf5f921b59864af8f3391610db
Larry Madeo <lmadeo76@gmail.com>
2017-06-21 06:14:07 +0000

    new function prototypes

--- holberton.h
+++ holberton.h
@@ -56,4 +56,7 @@ int choose_op(params_t *p, va_list valist);
 int init_params(params_t *p, const char *format, op_t *ops[]);
 char *_strdup(const char *src);
 
+/* math.c */
+int itoa(int n, char s[]);
+int _abs(int n);
 #endif /* HOLBERTON */
