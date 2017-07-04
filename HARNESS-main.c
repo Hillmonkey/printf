59418d2a992740271c1df8e685e171dd2fe5d7be
Larry Madeo <lmadeo76@gmail.com>
2017-07-04 16:09:32 -0700

    first commit

---
+++ HARNESS-main.c
@@ -0,0 +1,27 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point to use any test file in TEST-FILES folder
+ * Return: Always 0
+ * Note: compiler warnings from test files can be ignored
+ **/
+
+// USAGE: gcc -Wall -Wextra -pedantic *.c *.h ./TEST-FILES/*.c
+
+int main(void)
+{
+	baby_test();
+	count_test();
+	day2_test();
+	felicia_complete_test();
+	felicia_test();
+	int_test();
+	still_failing();
+	test_test();
+	tiniest_test();
+	tiny_test();
+
+	return (0);
+} 
